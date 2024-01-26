986b0e4e1db37318eddccfd85b038733ccf6dbdc
= <=>
2024-01-26 17:34:14 -0300

    Adicionado seção FAQ

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded' , function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(btn){
@@ -12,9 +13,22 @@ document.addEventListener('DOMContentLoaded' , function(){
             btn.target.classList.add('shows__tabs__button--is-active');
         })
     }
+    
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+
+    }
 })
 
-function removeActivedBtn(){
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+    console.log('foi');
+}
+
+function removeActivedBtn(elemento){
     const buttons = document.querySelectorAll('[data-tab-button]');
     
     for (let i = 0; i < buttons.length; i++){
