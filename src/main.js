63759d239e92341244677c72965bc88b3f093d54
= <=>
2024-01-29 17:44:54 -0300

    Complete header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded' , function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question]')
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementosHeader();
+            console.log(posicaoAtual);
+        }else{
+            exibeElementosHeader();
+        }
+    })
 
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(btn){
@@ -19,6 +33,16 @@ document.addEventListener('DOMContentLoaded' , function(){
 
     }
 })
+
+function ocultaElementosHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function abreOuFechaResposta(elemento){
     const classe = 'faq__questions__item--is-open';
